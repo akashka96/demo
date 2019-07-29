f916cdce0fd5ead83dbcde1e3ad48c36f785fb29
akashka96 <akashka003@gmail.com>
2019-07-29 14:33:18 +0530

    demo

--- dist/js/demo.js
+++ dist/js/demo.js
@@ -19,6 +19,7 @@ $(document).ready(function () {
   }); // Toggle plus minus icon on show hide of collapse element
 
   $(".collapse").on('show.bs.collapse', function () {
+    console.log('about to show');
     $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
   }).on('hide.bs.collapse', function () {
     $(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
